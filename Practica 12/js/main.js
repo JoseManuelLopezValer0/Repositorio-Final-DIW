cf7fdedbb98de58291b60a64220e69a03a09b9a6
Jose Manuel Lopez Valero <a22lovajo@iesgrancapitan.org>
2023-06-08 21:25:07 +0200

    change

--- Practica 12/js/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file main.js
- * @author Jose Manuel Lopez Valero
- */
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("boton").addEventListener("click", function () {
-    
-  });
-});
